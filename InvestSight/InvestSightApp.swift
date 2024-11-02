0608aff349a34765e150236f8d156d08218829e2
Tiago Afonso <88906334+Squidalula@users.noreply.github.com>
2024-11-02 20:13:10 +0000

    Initial Commit

---
+++ InvestSight/InvestSightApp.swift
@@ -0,0 +1,17 @@
+//
+//  InvestSightApp.swift
+//  InvestSight
+//
+//  Created by Tiago Afonso on 02/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct InvestSightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
